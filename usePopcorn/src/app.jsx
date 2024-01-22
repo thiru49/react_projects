b0c944a92a275725bfd917473c07a9225039874c
thiru49 <thiruppathi497@gmail.com>
2024-01-22 12:05:55 +0530

    build static components usepopcorn

--- usePopcorn/src/app.jsx
+++ usePopcorn/src/app.jsx
@@ -1,54 +1,15 @@
-import { useState } from "preact/hooks";
-import "./app.css";
-
 export function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <Rating />
+      <NavBar />
     </>
   );
 }
 
-const Rating = () => {
-  const [value, setValue] = useState(0);
-  const [mouse, setMouse] = useState(0);
+const NavBar = () => {
   return (
-    <div className="m-4 flex items-center">
-      {Array.from({ length: 10 }, (_, i) => (
-        <Rate
-          key={i + 1}
-          rating={() => setValue(i + 1)}
-          full={mouse ? mouse >= i + 1 : value >= i + 1}
-          onMoveon={() => setMouse(i + 1)}
-          onMoveOff={() => setMouse(0)}
-        />
-      ))}
-
-      <p className="ml-4 text-xl">{mouse || value || ""}</p>
+    <div>
+      <h1>Navbar</h1>
     </div>
-  );
-};
-
-const Rate = ({ rating, full, onMoveon, onMoveOff }) => {
-  return (
-    <>
-      {full ? (
-        <div
-          className="size-5 bg-yellow-300 mr-1"
-          onClick={rating}
-          onMouseEnter={onMoveon}
-          onMouseLeave={onMoveOff}
-        ></div>
-      ) : (
-        <div
-          className="size-5 bg-black mr-1"
-          onClick={rating}
-          onMouseEnter={onMoveon}
-          onMouseLeave={onMoveOff}
-        ></div>
-      )}
-    </>
   );
 };
