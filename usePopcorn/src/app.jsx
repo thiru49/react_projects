0eb0bb8695a777c728d168291cd5965790190626
thiru49 <thiruppathi497@gmail.com>
2024-01-22 17:24:53 +0530

    commponent composition

--- usePopcorn/src/app.jsx
+++ usePopcorn/src/app.jsx
@@ -50,131 +50,155 @@ export function App() {
   return (
     <>
       <NavBar />
-      <Main />
+      <Main>
+        <Box>
+          <MovieLists />
+        </Box>
+        <Box>
+          <WatchedSummary />
+          <WatchedMoviesLists />
+        </Box>
+      </Main>
     </>
   );
 }
 
 const NavBar = () => {
   return (
-    <div className="flex flex-row justify-between items-center px-10 py-2 bg-indigo-600 rounded-xl shadow-xl text-md">
-      <div className="flex justify-center items-center gap-2">
+    <div className="flex flex-row justify-between sm:m-4 items-center px-4 sm:px-10 py-2 bg-indigo-600 rounded-xl shadow-xl gap-2 text-xs sm:text-md">
+      <div className="flex sm:flex-row flex-col justify-center items-center gap-2">
         <span>🍿</span>
-        <h1 className="text-2xl font-bold">MoviePicker</h1>
+        <h1 className="sm:text-2xl font-bold">MoviePicker</h1>
       </div>
       <input
-        className="px-4 py-2 rounded-md hover:-translate-y-1 transition-all text-indigo-100 font-semibold border-none max-w-xl sm:min-w-[30rem] shadow-md focus:outline-none  bg-indigo-500"
+        className="px-4 py-2 rounded-md hover:-translate-y-1 transition-all text-indigo-100 font-semibold border-none  md:min-w-[30rem] shadow-md w-[150px]  focus:outline-none  bg-indigo-500"
         placeholder="Search Movies "
       />
-      <p>
+      <p className="font-bold">
         Found <strong>X</strong> results
       </p>
     </div>
   );
 };
 
-const Main = () => {
-  const [isOpen1, setIsOpen1] = useState(true);
-  const [isOpen2, setIsOpen2] = useState(true);
+const Main = ({ children }) => {
+  return (
+    <main className="mt-8  h-[70vh] flex sm:flex-row flex-col gap-2 sm:w-[700px] mx-auto">
+      {children}
+    </main>
+  );
+};
+
+const Box = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <main className="mt-8  h-[70vh] flex gap-2 w-[65%] mx-auto">
-      <div className="basis-1/2 relative overflow-auto bg-gray-700 rounded-xl shadow-xl">
-        <button className="bg-black size-8 rounded-full absolute right-2 top-2">
-          +
-        </button>
-        {isOpen1 && (
-          <ul className="">
-            {tempMovieData?.map((movie) => (
-              <li
-                key={movie.imdbID}
-                className="flex justify-start gap-8 px-4 py-4 items-center hover:bg-gray-600 border-b-2 border-gray-600"
-              >
-                <img
-                  src={movie.Poster}
-                  alt={`${movie.Title} poster`}
-                  className="size-14"
-                />
-                <div className="flex flex-col gap-2">
-                  <h3 className="text-xl font-sans leading-6 font-bold">
-                    {movie.Title}
-                  </h3>
-                  <div>
-                    <p>
-                      <span className="p-2 size-8 text-white">🗓</span>
-                      <span>{movie.Year}</span>
-                    </p>
-                  </div>
-                </div>
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
-      <div className=" basis-1/2 relative overflow-auto bg-gray-700 rounded-xl shadow-xl">
-        <button className="bg-black size-8 rounded-full absolute right-2 top-2">
-          +
-        </button>
-        {isOpen2 && (
-          <>
-            <div className="p-4 flex flex-col gap-4 bg-gray-900 rounded-xl shadow-xl">
-              <h2 className="text-xl font-bold ">Movies you watched</h2>
-              <div className="flex justify-around items-center">
-                <p className="flex justify-start items-center gap-2">
-                  <span>#️⃣</span>
-                  <span> movies</span>
-                </p>
-                <p className="mini">
-                  <span>⭐️</span>
-                  <span>X</span>
-                </p>
-                <p className="mini">
-                  <span>🌟</span>
-                  <span>Y</span>
-                </p>
-                <p className="mini">
-                  <span>⏳</span>
-                  <span>Z</span>
-                </p>
-              </div>
-            </div>
+    <div className="basis-1/2 relative overflow-auto bg-gray-700 rounded-xl shadow-xl">
+      <button
+        className="bg-black size-8 rounded-full absolute right-2 top-2"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {isOpen ? "-" : "+"}
+      </button>
+      {isOpen && children}
+    </div>
+  );
+};
 
-            <ul className="">
-              {tempWatchedData.map((movie) => (
-                <li
-                  key={movie.imdbID}
-                  className="flex justify-start gap-8 px-4 py-4 items-center hover:bg-gray-600 border-b-2 border-gray-600"
-                >
-                  <img
-                    src={movie.Poster}
-                    alt={`${movie.Title} poster`}
-                    className="size-14"
-                  />
-                  <div className="flex flex-col gap-2">
-                    <h3 className="text-xl font-sans leading-6 font-bold">
-                      {movie.Title}
-                    </h3>
-                    <div className="flex gap-2">
-                      <p className="mini">
-                        <span>⭐️</span>
-                        <span>{movie.imdbRating}</span>
-                      </p>
-                      <p className="mini">
-                        <span>🌟</span>
-                        <span>{movie.userRating}</span>
-                      </p>
-                      <p className="mini">
-                        <span>⏳</span>
-                        <span>{movie.runtime} min</span>
-                      </p>
-                    </div>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </>
-        )}
+const MovieLists = () => {
+  return (
+    <ul className="">
+      {tempMovieData?.map((movie) => (
+        <Movie movie={movie} key={movie.imdbID} />
+      ))}
+    </ul>
+  );
+};
+
+const Movie = ({ movie }) => {
+  return (
+    <li
+      key={movie.imdbID}
+      className="flex justify-start gap-8 px-4 py-4 items-center hover:bg-gray-600 border-b-2 border-gray-600"
+    >
+      <img
+        src={movie.Poster}
+        alt={`${movie.Title} poster`}
+        className="size-14"
+      />
+      <div className="flex flex-col gap-2">
+        <h3 className="text-xl font-sans leading-6 font-bold">{movie.Title}</h3>
+        <div>
+          <p>
+            <span className="p-2 size-8 text-white">🗓</span>
+            <span>{movie.Year}</span>
+          </p>
+        </div>
       </div>
-    </main>
+    </li>
+  );
+};
+
+const WatchedSummary = () => {
+  return (
+    <div className="p-4 flex flex-col gap-4 bg-gray-900 rounded-xl shadow-xl">
+      <h2 className="text-xl font-bold ">Movies you watched</h2>
+      <div className="flex justify-around items-center">
+        <p className="flex justify-start items-center gap-2">
+          <span>#️⃣</span>
+          <span> movies</span>
+        </p>
+        <p className="mini">
+          <span>⭐️</span>
+          <span>X</span>
+        </p>
+        <p className="mini">
+          <span>🌟</span>
+          <span>Y</span>
+        </p>
+        <p className="mini">
+          <span>⏳</span>
+          <span>Z</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+const WatchedMoviesLists = () => {
+  return (
+    <ul className="">
+      {tempWatchedData.map((movie) => (
+        <li
+          key={movie.imdbID}
+          className="flex justify-start gap-8 px-4 py-4 items-center hover:bg-gray-600 border-b-2 border-gray-600"
+        >
+          <img
+            src={movie.Poster}
+            alt={`${movie.Title} poster`}
+            className="size-14"
+          />
+          <div className="flex flex-col gap-2">
+            <h3 className="text-xl font-sans leading-6 font-bold">
+              {movie.Title}
+            </h3>
+            <div className="flex gap-2">
+              <p className="mini">
+                <span>⭐️</span>
+                <span>{movie.imdbRating}</span>
+              </p>
+              <p className="mini">
+                <span>🌟</span>
+                <span>{movie.userRating}</span>
+              </p>
+              <p className="mini">
+                <span>⏳</span>
+                <span>{movie.runtime} min</span>
+              </p>
+            </div>
+          </div>
+        </li>
+      ))}
+    </ul>
   );
 };
