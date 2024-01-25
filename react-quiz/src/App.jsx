2e77b6414e2809d9e651e621ae748eff18ffbdbb
thiru49 <thiruppathi497@gmail.com>
2024-01-25 15:59:38 +0530

    loading questions from a fake api

--- react-quiz/src/App.jsx
+++ react-quiz/src/App.jsx
@@ -1,8 +1,35 @@
-import { useState } from "react";
+import { useEffect, useReducer } from "react";
 import Main from "./components/Main";
 import { Header } from "./components/Header";
 
+const initialState = {
+  questions: [],
+  //'loading,'error','ready','active','finished'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecevied":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("action unknown");
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  console.log(state);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataRecevied", payload: data }))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="app">
       <Header />
--- react-quiz/src/components/Header.jsx
+++ react-quiz/src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import logo from "../assets/logo192.png";
 
 export const Header = () => {
   return (
     <header className="app-header">
-      <img src="../assets/react.svg" alt="React logo" />
+      <img src={logo} alt="React logo" />
       <h1>The React Quiz</h1>
     </header>
   );
