e43902ea8780b64624db7f6caf5bb7c599a9dba4
thiru49 <thiruppathi497@gmail.com>
2024-01-29 21:53:18 +0530

    setup ake authentication context

---
+++ worldwise/src/context/FakeAuthContext.jsx
@@ -0,0 +1,72 @@
+import { useContext, useReducer } from "react";
+
+const AuthContext = useContext();
+
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+};
+const FAKE_USER = {
+  name: "Jack",
+  email: "jack@example.com",
+  password: "qwerty",
+  avatar: "https://i.pravatar.cc/100?u=zz",
+};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "login":
+      return {
+        ...state,
+        user: action.payload,
+        isAuthenticated: true,
+      };
+    case "logout":
+      return {
+        ...state,
+        user: null,
+        isAuthenticated: false,
+      };
+    default:
+      throw new Error("action unkown");
+  }
+};
+const AuthProvider = ({ children }) => {
+  const [{ user, isAuthenticated }, dispatch] = useReducer(
+    action,
+    initialState
+  );
+
+  const login = (email, password) => {
+    if (email === FAKE_USER.email && password === FAKE_USER.password)
+      dispatch({
+        type: "login",
+        payload: {
+          name: FAKE_USER.name,
+          email: FAKE_USER.email,
+        },
+      });
+  };
+  const logout = (email, password) => {
+    dispatch({ type: "logout" });
+  };
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        isAuthenticated,
+        login,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (context === undefined)
+    throw new Error("Authcontext was used outside of the AuthProvider");
+  return context;
+};
+export { AuthProvider, useAuth };
