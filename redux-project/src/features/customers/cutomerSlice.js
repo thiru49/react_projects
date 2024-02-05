7df5ff89944915a589bc3567d5980bd2f19e5ef1
thiru49 <thiruppathi497@gmail.com>
2024-02-05 17:21:25 +0530

    creating customer slice

--- redux-project/src/features/customers/cutomerSlice.js
+++ redux-project/src/features/customers/cutomerSlice.js
@@ -1,10 +1,39 @@
-const initialStateCustomer = {
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
   fullName: "",
   nationalID: "",
   createdAt: "",
 };
 
-function customerReducer(state = initialStateCustomer, action) {
+const customerSlice = createSlice({
+  name: "customer",
+  initialState,
+  reducers: {
+    createCustomer: {
+      prepare(fullName, nationalID) {
+        return {
+          payload: {
+            fullName,
+            nationalID,
+            createdAt: new Date().toISOString(),
+          },
+        };
+      },
+      reducer(state, action) {
+        state.fullName = action.payload.fullName;
+        state.nationalID = action.payload.nationalID;
+        state.createdAt = action.payload.createdAt;
+      },
+    },
+    updateName(state, action) {
+      state.fullName = action.payload;
+    },
+  },
+});
+export const { createCustomer, updateName } = customerSlice.actions;
+export default customerSlice.reducer;
+/* function customerReducer(state = initialStateCustomer, action) {
   switch (action.type) {
     case "customer/createCustomer":
       return {
@@ -31,4 +60,4 @@ function updateName(fullName) {
   return { type: "customer/updateName", payload: fullName };
 }
 
-export { updateName, createCustomer, customerReducer };
+export { updateName, createCustomer, customerReducer }; */
--- redux-project/src/store.js
+++ redux-project/src/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import accountReducer from "./features/accounts/accountSlice";
-import { customerReducer } from "./features/customers/cutomerSlice";
+import customerReducer from "./features/customers/cutomerSlice";
 
 const store = configureStore({
   reducer: {
