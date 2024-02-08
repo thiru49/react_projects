71a4427d3323716a1b891f5b0a438f740620bc4d
thiru49 <thiruppathi497@gmail.com>
2024-02-08 11:31:24 +0530

    error data with form action

--- fast_react_go/src/features/order/CreateOrder.jsx
+++ fast_react_go/src/features/order/CreateOrder.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Form, redirect } from "react-router-dom";
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
@@ -34,6 +34,10 @@ const fakeCart = [
 
 function CreateOrder() {
   // const [withPriority, setWithPriority] = useState(false);
+  const navigation = useNavigation();
+  const formError = useActionData();
+
+  const isSubmitting = navigation.state === "submitting";
   const cart = fakeCart;
 
   return (
@@ -51,6 +55,7 @@ function CreateOrder() {
           <div>
             <input type="tel" name="phone" required />
           </div>
+          <div>{formError && <p>{formError.phone}</p>}</div>
         </div>
 
         <div>
@@ -73,7 +78,9 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button>Order now</button>
+          <button disabled={isSubmitting}>
+            {isSubmitting ? "...Placing Orders" : "Order now"}
+          </button>
         </div>
       </Form>
     </div>
@@ -83,12 +90,19 @@ function CreateOrder() {
 export async function action({ request }) {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
-  console.log(data);
+
   const order = {
     ...data,
     cart: JSON.parse(data.cart),
     priority: data.priority === "on",
   };
+
+  const errors = {};
+  if (!isValidPhone(order.phone))
+    errors.phone =
+      "Please give us your correct phone number.we might need it to contact you";
+  if (Object.keys(errors).length > 0) return errors;
+
   const newOrder = await createOrder(order);
   console.log(newOrder);
   return redirect(`/order/${newOrder.id}`);
