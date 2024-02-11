983e8bb74de0211da358c0059246aeda79d0e0e1
thiru49 <thiruppathi497@gmail.com>
2024-02-11 16:18:04 +0530

    modeleing cart state

---
+++ fast_react_go/src/features/cart/cartSlice.js
@@ -0,0 +1,41 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cart: [
+    {
+      pizzaId: 12,
+      name: "Mediterranean",
+      quantity: 2,
+      unitPrice: 16,
+      totalPrice: 32,
+    },
+  ],
+};
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      state.cart.push(action.payload);
+    },
+    deleteItem(state, action) {
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+    },
+    increaseItem(state, action) {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity++;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+    decreaseItem(state, action) {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity--;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+    clearCart(state, action) {
+      state.cart = [];
+    },
+  },
+});
+export const { addItem, decreaseItem, increaseItem, deleteItem } =
+  cartSlice.actions;
+export default cartSlice.reducer;
