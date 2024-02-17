d856926290909f057b0d1e3518d68143a6c46124
thiru49 <thiruppathi497@gmail.com>
2024-02-17 23:29:20 +0530

    building more resulble components

--- Wild_Oasis/src/App.jsx
+++ Wild_Oasis/src/App.jsx
@@ -1,10 +1,24 @@
+import styled from "styled-components";
 import { GlobalStyles } from "./styles/GlobalStyles";
+import Row from "./ui/Row";
+import Button from "./ui/Button";
+import Heading from "./ui/Heading";
+
+const StyledApp = styled.main``;
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <h1>welcome</h1>
+      <StyledApp>
+        <Row type="horizontal">
+          <Heading>The Wild Oasis</Heading>
+          <div>
+            <Button>checkIn</Button>
+            <Button>checkOut</Button>
+          </div>
+        </Row>
+      </StyledApp>
     </>
   );
 }
---
+++ Wild_Oasis/src/ui/Heading.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const Heading = styled.h1`
+  font-size: 20px;
+`;
+
+export default Heading;
